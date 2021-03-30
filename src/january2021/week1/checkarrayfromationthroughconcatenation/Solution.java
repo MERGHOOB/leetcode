74a53b1d9d1f0a25dcc6b02e592838be0500300d
mekhan <margub.khan@gmail.com>
2021-03-30 09:40:47 +0530

    checkarrayfromationthroughconcatenation findacopyoftargetnodeincloneofthattree

---
+++ src/january2021/week1/checkarrayfromationthroughconcatenation/Solution.java
@@ -0,0 +1,75 @@
+package january2021.week1.checkarrayfromationthroughconcatenation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You are given an array of distinct integers arr and an array of integer arrays pieces, where the integers in pieces are distinct. Your goal is to form arr by concatenating the arrays in pieces in any order. However, you are not allowed to reorder the integers in each array pieces[i].
+ * <p>
+ * Return true if it is possible to form the array arr from pieces. Otherwise, return false.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: arr = [85], pieces = [[85]]
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: arr = [15,88], pieces = [[88],[15]]
+ * Output: true
+ * Explanation: Concatenate [15] then [88]
+ * Example 3:
+ * <p>
+ * Input: arr = [49,18,16], pieces = [[16,18,49]]
+ * Output: false
+ * Explanation: Even though the numbers match, we cannot reorder pieces[0].
+ * Example 4:
+ * <p>
+ * Input: arr = [91,4,64,78], pieces = [[78],[4,64],[91]]
+ * Output: true
+ * Explanation: Concatenate [91] then [4,64] then [78]
+ * Example 5:
+ * <p>
+ * Input: arr = [1,3,5,7], pieces = [[2,4,6,8]]
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= pieces.length <= arr.length <= 100
+ * sum(pieces[i].length) == arr.length
+ * 1 <= pieces[i].length <= arr.length
+ * 1 <= arr[i], pieces[i][j] <= 100
+ * The integers in arr are distinct.
+ * The integers in pieces are distinct (i.e., If we flatten pieces in a 1D array, all the integers in this array are distinct).
+ */
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], i);
+        }
+
+        for (int[] piece : pieces) {
+            int lastIndex = -1;
+            for (int pieceElement : piece) {
+
+                if (map.containsKey(pieceElement)) {
+                    if (lastIndex == -1) {
+                        lastIndex = map.get(pieceElement);
+                    } else {
+                        if (map.get(pieceElement) == lastIndex + 1) {
+                            lastIndex += 1;
+                        } else {
+                            return false;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
