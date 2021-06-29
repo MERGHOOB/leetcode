540e4470dbaab5db23c39dd6cc1936ee342cafab
mekhan <margub.khan@gmail.com>
2021-06-29 12:16:00 +0530

    src\maximumlengthofpairchain Solution.java

---
+++ practice-leetcode/src/maximumlengthofpairchain/Solution.java
@@ -0,0 +1,56 @@
+package maximumlengthofpairchain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public int findLongestChainUsingGreedy(int[][] pairs) {
+
+        Arrays.sort(pairs, Comparator.comparingInt(o -> o[1]));
+
+        int len = 0;int curr = Integer.MIN_VALUE;
+        for(int[] pair: pairs) {
+            if(curr < pair[0]) {
+                curr = pair[1];
+                len++;
+            }
+        }
+
+     return len;
+    }
+
+    public int findLongestChain(int[][] pairs) {
+
+        Arrays.sort(pairs, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+            return Integer.compare(o1[1], o2[1]);
+        });
+
+        int[] dp = new int[pairs.length];
+        Arrays.fill(dp, 1);
+        int len = 1;
+
+        for (int i = 1; i < pairs.length; i++) {
+
+            for (int j = i - 1; j >= 0; j--) {
+
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+
+
+            }
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            len = Math.max(len, dp[i]);
+        }
+        return len;
+    }
+
+}
