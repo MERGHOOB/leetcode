d97ff384f869b58b74d1e45acf5767b7258c166d
mekhan <margub.khan@gmail.com>
2021-09-20 19:33:01 +0530

    amazon\leetcode_median_finder

--- companies/amazon/leetcode_median_finder/MedianFinder.java
+++ companies/amazon/leetcode_median_finder/MedianFinder.java
@@ -48,9 +48,9 @@ class MedianFinder {
     }
 }
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
+/*
+  Your MedianFinder object will be instantiated and called as such:
+  MedianFinder obj = new MedianFinder();
+  obj.addNum(num);
+  double param_2 = obj.findMedian();
  */
